7d857257f16278f3b895456d30287203a847afbe
Elvis-Munene <104641808+Elvis-Munene@users.noreply.github.com>
2023-05-23 09:15:41 +0300

    quotes the sql statement

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.get("/", function(req, res){
 
 
 app.get("/export", function(request, response, next){
-   connection.query(SELECT * FROM  aws_Analysis, function(error, data){
+   connection.query("SELECT * FROM  aws_Analysis", function(error, data){
     const mysql_data = JSON.parse(JSON.stringify(data));
 
     const file_header = ['temperature', 'humidity', 'record_date'];
