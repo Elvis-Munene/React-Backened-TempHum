6914479cddc5b87df1a4d3ff3f1ca8a39b0b5cdd
Elvis-Munene <104641808+Elvis-Munene@users.noreply.github.com>
2023-05-23 08:54:14 +0300

    Update database.js to AWS

--- database.js
+++ database.js
@@ -10,11 +10,20 @@ const mysql = require('mysql2');
 // })
 
 
+// const connection = mysql.createConnection({
+//     host:"sql10.freesqldatabase.com",
+//     user:"sql10608218",
+//     password:"unt1JhpIgM",
+//     database:"sql10608218"
+// })
+
 const connection = mysql.createConnection({
-    host:"sql10.freesqldatabase.com",
-    user:"sql10608218",
-    password:"unt1JhpIgM",
-    database:"sql10608218"
+    host: "temp-humid-2.cpjz7irotnnx.us-west-2.rds.amazonaws.com",
+    port: 3306,
+    user: "admin",
+    password: "admin1234",
+    database: "myarduinodatabase"
+
 })
 
 
